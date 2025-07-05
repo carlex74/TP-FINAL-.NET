fb0a0484e7bab6d0c85c8f1f710e2f7ddb720f49
carlex <ricardogugliermino@gmail.com>
2025-07-05 17:10:02 -0300

    Creacion de clases iniciales

---
+++ Domain.Model/BusinessEntity.cs
@@ -0,0 +1,21 @@
+﻿namespace Domain.Model
+{
+    public class BusinessEntity
+    {
+        private int _ID;
+        private string _Stete;
+
+        public int ID { get { return _ID; } }
+
+        public string Stete { get { return _Stete; } }
+    
+        
+        public void businessEntity() { 
+        
+        
+        }
+    
+    }
+
+
+}
