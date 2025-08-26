71f2e368b091afd358659cbce697040b509fda0c
carlex <ricardogugliermino@gmail.com>
2025-08-26 11:15:56 -0300

    Feature:Comienzo de configuracion de EF

---
+++ Infrastructure/Context/TpConterxt.cs
@@ -0,0 +1,32 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.EntityFrameworkCore.SqlServer;
+
+namespace Infrastructure.Context
+{
+    public class TpConterxt : DbContext
+    {
+        public DbSet<Plan> plan { get; set; }
+        public DbSet<Especialidad> especialidad { get; set; }
+
+        internal TpConterxt()
+        {
+            this.Database.EnsureCreated();
+        }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+            if (!optionsBuilder.IsConfigured)
+            {
+                var configuration = new ConfigurationBuilder()
+                    .SetBasePath(Directory.GetCurrentDirectory()) // Ensure System.IO is included
+                    .AddJsonFile("appsettings.json", optional: false, reloadOnChange: true)
+                    .Build();
+
+                string connectionString = configuration.GetConnectionString("DefaultConnection");
+                optionsBuilder.UseSqlServer(connectionString); // This requires the Microsoft.EntityFrameworkCore.SqlServer package
+            }
+        }
+    }
+}
